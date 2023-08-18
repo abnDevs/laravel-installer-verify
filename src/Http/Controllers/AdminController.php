2f359de6f9678512322994443a2e1d4a1e3ed08e
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2023-08-18 13:58:35 +0600

    Add try catch

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -43,19 +43,23 @@ class AdminController extends Controller
 
     public function store(StoreAdminRequest $request)
     {
-        $superAdmin = User::create([
-            'name' => $request->validated('name'),
-            'email' => $request->validated('email'),
-            'password' => bcrypt($request->validated('password')),
-            ...config('installer.admin.extra')
-        ]);
+        try {
+            $superAdmin = User::create([
+                'name' => $request->validated('name'),
+                'email' => $request->validated('email'),
+                'password' => bcrypt($request->validated('password')),
+                ...config('installer.admin.extra')
+            ]);
 
-        if (config('installer.admin.has_role')) {
-            if (! Role::where('name', config('installer.admin.role', 'Super Admin'))->exists()) {
-                Role::create(['name' => config('installer.admin.role')]);
+            if (config('installer.admin.has_role')) {
+                if (! Role::where('name', config('installer.admin.role', 'Super Admin'))->exists()) {
+                    Role::create(['name' => config('installer.admin.role')]);
+                }
+
+                $superAdmin->assignRole(config('installer.admin.role'));
             }
-
-            $superAdmin->assignRole(config('installer.admin.role'));
+        }catch (\Exception $exception){
+            return error($exception->getMessage());
         }
 
         Cache::put('installer.admin', true);
